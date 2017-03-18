b9a80c1dc6baad9d3b4b4ba556dd7993299882d1
qi.li <qi.li@flightroutes24.com>
2017-03-19 02:33:57 +0800

    注解

--- src/main/java/com/website/learn/service/impl/LoginServiceImpl.java
+++ src/main/java/com/website/learn/service/impl/LoginServiceImpl.java
@@ -1,12 +1,14 @@
 package com.website.learn.service.impl;
 
 import com.website.learn.service.LoginService;
+import org.springframework.stereotype.Service;
 
 /**
  * @author dell
  * @since 1.0.0
  * Created On 2017-03-19 02:29
  */
+@Service
 public class LoginServiceImpl implements LoginService {
     @Override
     public void login() {
